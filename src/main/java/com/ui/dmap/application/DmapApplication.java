c88f3dec2e6210c9fcad7ca9e963c78e4649fabf
abhandari88 <amit.bhandari@cubic.com>
2018-11-28 17:54:31 -0500

    Initial commit

---
+++ src/main/java/com/ui/dmap/application/DmapApplication.java
@@ -0,0 +1,12 @@
+package com.ui.dmap.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmapApplication.class, args);
+    }
+}
